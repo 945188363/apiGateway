7c2690bf98e49d77cc02237cfc00681bb58ffdd2
945188363 <niujingheng1997@163.com>
2020-05-16 17:33:43 +0800

    钉钉告警接入 Prometheus监控接入

---
+++ Utils/AlertAlarm.go
@@ -0,0 +1,31 @@
+package Utils
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const Webhook_url = "https://oapi.dingtalk.com/robot/send?access_token=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+func AlertDingAlarm(msg string) bool {
+	content, data := make(map[string]string), make(map[string]interface{})
+	content["content"] = msg
+	// 可以选择markdown格式
+	data["msgtype"] = "text"
+	data["text"] = content
+	b, _ := json.Marshal(data)
+
+	resp, err := http.Post(Webhook_url,
+		"application/json",
+		bytes.NewBuffer(b))
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+	return true
+}
